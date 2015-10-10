35b65b344acbc2046d02758cb21175864ea90189
Lauren Fay <lfr2lsr@gmail.com>
2015-10-10 21:05:56 +0200

    Update right_to_left.py

--- elementary/right_to_left.py
+++ elementary/right_to_left.py
@@ -1,6 +1,7 @@
 """
 
 DIFFICULTY: ELEMENTARY
+
 One of robots is charged with a simple task: to join a sequence of strings in one sentence to produce instructions on how to get around the ship. But this robot is left-hander and has a tendency to joke around and confuse its right handed friends.
 You are given a sequence of strings. You should join these strings into chunk of text where the initial strings are separated by commas. As a joke on the right handed robots, you should replace all cases of the words "right" with the word "left", even if it's a part of another word. All strings are given in lowercase.
 Input: A sequence of strings as a tuple of strings (unicode).
