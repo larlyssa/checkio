6305a97ce064942d8cc94a74ed783ec889e813ad
Lauren Fay <lfr2lsr@gmail.com>
2015-10-10 21:04:01 +0200

    Update even_the_last.py

--- elementary/even_the_last.py
+++ elementary/even_the_last.py
@@ -1,4 +1,7 @@
 """
+
+DIFFICULTY: ELEMENTARY
+
 You are given an array of integers. You should find the sum of the elements with even indexes (0th, 2nd, 4th...),
 then multiply this summed number and the final element of the array together. 
 Don't forget that the first element has an index of 0.
