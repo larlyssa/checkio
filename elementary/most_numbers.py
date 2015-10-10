fd988e13944f1bca709e5c25c4c21ccb585980de
Lauren Fay <lfr2lsr@gmail.com>
2015-10-10 21:05:32 +0200

    Update most_numbers.py

--- elementary/most_numbers.py
+++ elementary/most_numbers.py
@@ -1,4 +1,7 @@
 """
+
+DIFFICULTY: ELEMENTARY
+
 Let's work with numbers.
 You are given an array of numbers (floats). You should find the difference between the maximum and minimum element.
 Your function should be able to handle an undefined amount of arguments. 
